6d519dcc029c1785aa5a76d037fb7c79777ed2ef
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-05-23 23:16:38 +0530

    new one

--- src/components/AceternityUI/3D-cardEffect/3d-box.jsx
+++ src/components/AceternityUI/3D-cardEffect/3d-box.jsx
@@ -9,7 +9,7 @@ export function BackgroundBoxesDemo() {
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
-      <h1 className={cn("spText text-4xl lg:text-5xl bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text relative z-20")}>
+      <h1 className={cn("spText flex justify-center text-center text-4xl lg:text-5xl bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text relative z-20")}>
       FULLSTACK WEB DEVELOPER
       </h1>
       
